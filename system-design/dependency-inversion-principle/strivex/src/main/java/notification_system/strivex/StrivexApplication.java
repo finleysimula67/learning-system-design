a9d577f6e9b056584f53d6ef64dc06daacdde0f1
Suckscorpio999 <olinabin214@gmail.com>
2026-05-17 19:34:30 +0545

    feat: added Main Application to run the application

---
+++ system-design/dependency-inversion-principle/strivex/src/main/java/notification_system/strivex/StrivexApplication.java
@@ -0,0 +1,13 @@
+package notification_system.strivex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrivexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrivexApplication.class, args);
+	}
+
+}
